6c29a7bcecd87eed57718efcd3efd9c4b40d3fc3
kaitou141 <hdk18112003@gmail.com>
2025-02-12 12:42:37 +0700

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,55 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+          if (id.includes("@src/app")) {
+            return "app";
+          }
+          if (id.includes("@src/components")) {
+            return "components";
+          }
+          if (id.includes("@src/context")) {
+            return "context";
+          }
+          if (id.includes("@src/const")) {
+            return "const";
+          }
+          if (id.includes("@src/utils")) {
+            return "utils";
+          }
+          if (id.includes("@src/routes")) {
+            return "routes";
+          }
+          if (id.includes("@src/hooks")) {
+            return "hooks";
+          }
+          if (id.includes("@src/pages")) {
+            return "pages";
+          }
+          if (id.includes("@src/layout")) {
+            return "layout";
+          }
+          if (id.includes("@src/models")) {
+            return "models";
+          }
+          if (id.includes("@src/services")) {
+            return "services";
+          }
+        }
+      },
+      // debug lottie warning
+      onwarn(warning, warn) {
+        // Suppress eval warnings
+        if (warning.code === "EVAL") return;
+        warn(warning);
+      }
+    },
+    chunkSizeWarningLimit: 10000
+  }
 });
