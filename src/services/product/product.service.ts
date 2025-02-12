cd4ffef47b0cc3edc92bc9aa2cd4188bdb18b429
server-craftsman <huyit2003@gmail.com>
2025-02-12 09:58:56 +0700

    [NguyenDanHuy_12.02.2025] refactor: update response interfaces and notification helper colors

--- src/services/product/product.service.ts
+++ src/services/product/product.service.ts
@@ -1,11 +1,11 @@
 import { BaseService } from "../config/base.service";
 import { API_PATH } from "../../const";
-import { ResponseSuccess } from "../../app/interface/response_success.interface";
+import { ResponseSuccessForList } from "../../app/interface/response_success.interface";
 import { GetAllCategoryResponseModel } from "../../models/api/response/product.res.model";
 
 export const ProductService = {
   getAll(params: any) {
-    return BaseService.get<ResponseSuccess<GetAllCategoryResponseModel>>({
+    return BaseService.get<ResponseSuccessForList<GetAllCategoryResponseModel>>({
       url: API_PATH.PRODUCT.GET_ALL,
       payload: params
     });
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -11,7 +11,7 @@ export const notificationMessage = (message: string, type: "success" | "error" |
       draggable: true,
       progress: undefined,
       theme: "colored",
-      style: { backgroundColor: type === "success" ? "#1a237e" : type === "error" ? "#d32f2f" : type === "info" ? "#1a237e" : type === "warning" ? "#ff9800" : "#ffffff" }
+      style: { backgroundColor: type === "success" ? "green" : type === "error" ? "red" : type === "info" ? "blue" : type === "warning" ? "yellow" : "#ffffff" }
     };
   
     return type === "success" ? toast.success(message, options as ToastOptions<unknown>) : type === "error" ? toast.error(message, options as ToastOptions<unknown>) : type === "info" ? toast.info(message, options as ToastOptions<unknown>) : toast.warning(message, options as ToastOptions<unknown>);
