d16b944f861ddafd5e763293153cad1953ce5629
server-craftsman <huyit2003@gmail.com>
2025-03-04 11:32:11 +0700

    [NguyenDanHuy_04.03.2025] feat: config req/ res for campaign stage in Stage Service

---
+++ src/models/modules/Campaign.ts
@@ -0,0 +1,11 @@
+export interface Campaign {
+  id: string;
+  name: string;
+  startDate: string;
+  endDate: string;
+  quantitySold: number;
+  targetQuantity: number;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+}
--- src/services/campaign/stage.service.ts
+++ src/services/campaign/stage.service.ts
@@ -1,13 +1,37 @@
 import { BaseService } from '../config/base.service';
 import { API_PATH } from '../../const';
-import {
-  formatResponseSuccess
-} from '../../app/interface/response_success.interface';
+import { formatResponseSuccess } from '../../app/interface/response_success.interface';
+import { CreateCampaignStageRequestModel } from '../../models/api/request/campaign.req.model';
+import { CampaignStageResponseModel } from '../../models/api/response/campaign.res.model';
 
 export const StageService = {
+  create(params: CreateCampaignStageRequestModel, id: string) {
+    return BaseService.post<formatResponseSuccess<CampaignStageResponseModel>>({
+      url: API_PATH.CAMPAIGN_STAGE.BASE.replace(':id', id),
+      payload: params,
+    });
+  },
   getAll(params: any) {
-    return BaseService.get<formatResponseSuccess<any>>({
+    return BaseService.get<formatResponseSuccess<CampaignStageResponseModel>>({
       url: API_PATH.CAMPAIGN_STAGE.BASE,
+      payload: params,
+    });
+  },
+  update(params: CreateCampaignStageRequestModel, id: string, stageId: string) {
+    return BaseService.put<formatResponseSuccess<CampaignStageResponseModel>>({
+      url: API_PATH.CAMPAIGN_STAGE.UPDATE.replace(':id', id).replace(
+        ':stageId',
+        stageId
+      ),
+      payload: params,
+    });
+  },
+  delete(params: any, id: string, stageId: string) {
+    return BaseService.remove<formatResponseSuccess<null>>({
+      url: API_PATH.CAMPAIGN_STAGE.DELETE.replace(':id', id).replace(
+        ':stageId',
+        stageId
+      ),
       payload: params,
     });
   },
